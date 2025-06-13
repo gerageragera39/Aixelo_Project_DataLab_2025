2224f285b169efd7248a749ba4b4658daf76686b
gerageragera39 <german030103@gmail.com>
2025-06-13 12:42:36 +0200

    fingerprints and models training

---
+++ src/fingerprints/MatchData.py
@@ -0,0 +1,41 @@
+import csv
+import os
+import re
+
+data_dict = {}
+with open('../../data/odac/promising_mof/promising_mof_energies/adsorption_energy.txt', "r") as f:
+    for line in f:
+        key, value = line.strip().split()
+        data_dict[key.strip()] = float(value)
+
+print(f"Length of adsorption_energy.txt : {len(data_dict)} ")
+
+
+def find_matching_cif_files(root_dir):
+    pattern = re.compile(r"_(\d+)_relaxed\.cif$")
+    matching_cif_files = []
+
+    for subdir, dirs, files in os.walk(root_dir):
+        for file in files:
+            if file.endswith(".cif") and pattern.search(file):
+                name_without_ext = os.path.splitext(file)[0]
+                for key in data_dict:
+                    if name_without_ext.startswith(key):
+                        energy = data_dict[key]
+                        full_file_path = os.path.join(subdir, file)
+                        matching_cif_files.append((full_file_path, energy))
+                        break
+    return matching_cif_files
+
+
+root_dir_CO2 = '../../data/odac/pristine_CO2'
+matching_cif_files_CO2 = find_matching_cif_files(root_dir_CO2)
+
+root_dir_H2O = '../../data/odac/pristine_H2O'
+matching_cif_files_H2O = find_matching_cif_files(root_dir_H2O)
+
+output_full = "matched_cifs_with_energy.csv"
+with open(output_full, mode='w', newline='', encoding='utf-8') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(['file_path', 'energy'])
+    writer.writerows(matching_cif_files_CO2 + matching_cif_files_H2O)
---
+++ src/fingerprints/generators/45FP/45FingerprintsGenerator.py
@@ -0,0 +1,91 @@
+import os
+import pandas as pd
+import numpy as np
+from pymatgen.core import Structure
+from pymatgen.core.periodic_table import Element
+from scipy.stats import gmean
+from tqdm import tqdm
+import re
+
+property_mapping = {
+    "atomic_num": "Z",
+    "group": "group",
+    "period": "row",
+    "electronegativity": "X",
+    "electron_affinity": "electron_affinity",
+    "melting_point": "melting_point",
+    "boiling_point": "boiling_point",
+    "density": "density_of_solid",
+    "ionization_energy": "ionization_energies"
+}
+stat_names = ["mean", "geometric_mean", "standard_deviation", "max", "min"]
+
+
+def compute_stats(values):
+    values = np.array(values, dtype=np.float64)
+    values = values[~np.isnan(values)]
+    if len(values) == 0:
+        return [np.nan] * 5
+    return [
+        np.mean(values),
+        gmean(values) if np.all(values > 0) else np.nan,
+        np.std(values),
+        np.max(values),
+        np.min(values)
+    ]
+
+
+df = pd.read_csv('../../matched_cifs_with_energy.csv')
+fingerprints = []
+
+for path in tqdm(df["file_path"]):
+    path = '../../' + path
+    try:
+        structure = Structure.from_file(path)
+        elements = [site.specie for site in structure if isinstance(site.specie, Element)]
+        stats = []
+        for prop_key, attr in property_mapping.items():
+            values = []
+            for el in elements:
+                try:
+                    if attr == "ionization_energies":
+                        val = el.ionization_energies[0] if el.ionization_energies else np.nan
+                    else:
+                        val = getattr(el, attr, np.nan)
+                    values.append(val)
+                except Exception:
+                    values.append(np.nan)
+            stats.extend(compute_stats(values))
+    except Exception as e:
+        print(f"Error with file : {path}: {e}")
+        stats = [np.nan] * 45
+    fingerprints.append(stats)
+
+columns = [f"{prop}_{stat}" for prop in property_mapping.keys() for stat in stat_names]
+
+filenames_only = df["file_path"].apply(os.path.basename)
+
+fingerprints_df = pd.DataFrame(fingerprints, columns=columns)
+result = pd.concat([filenames_only.rename("filename"), df["energy"].reset_index(drop=True), fingerprints_df], axis=1)
+
+result.to_csv("45_fingerprints_with_energy.csv", index=False)
+
+# ----------------------------------------------------------------------------------------
+
+df = pd.read_csv("45_fingerprints_with_energy.csv")
+
+
+def extract_base_mof_name(filename):
+    return re.sub(r'(_random_\d+|_\d+)?_relaxed\.cif$', '', filename)
+
+
+df['MOF'] = df['filename'].map(extract_base_mof_name)
+
+fingerprint_cols = [col for col in df.columns if col not in ['energy', 'filename', 'MOF']]
+
+agg_dict = {col: 'first' for col in fingerprint_cols}
+agg_dict['energy'] = 'mean'
+
+df_grouped = df.groupby('MOF').agg(agg_dict).reset_index()
+
+df_grouped.to_csv("45_fingerprints_grouped_avg_energy.csv", index=False)
